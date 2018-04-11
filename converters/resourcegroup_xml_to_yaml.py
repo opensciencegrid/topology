13fc3229ea5edc61a4c319374d08a806e557b1a6
Matyas Selmeci <matyas@cs.wisc.edu>
2018-04-11 18:24:54 -0500

    Simplify converted yml files and reproduce XML contents when converting back
    Note that attribute ordering is lost.

--- converters/resourcegroup_xml_to_yaml.py
+++ converters/resourcegroup_xml_to_yaml.py
@@ -1,7 +1,11 @@
 import anymarkup
+import copy
 import os
+import pprint
 import shutil
-
+import sys
+from collections import OrderedDict
+from typing import Dict, List, Union
 
 # Example URL: https://myosg.grid.iu.edu/rgsummary/xml?summary_attrs_showhierarchy=on&summary_attrs_showwlcg=on&summary_attrs_showservice=on&summary_attrs_showfqdn=on&summary_attrs_showvoownership=on&summary_attrs_showcontact=on&gip_status_attrs_showtestresults=on&downtime_attrs_showpast=&account_type=cumulative_hours&ce_account_type=gip_vo&se_account_type=vo_transfer_volume&bdiitree_type=total_jobs&bdii_object=service&bdii_server=is-osg&all_resources=on&facility_sel%5B%5D=10009&gridtype=on&gridtype_1=on&active=on&active_value=1&disable_value=1
 # Layout in that URL is
@@ -22,7 +26,13 @@ import shutil
 # named after the facility, site, and resource group (sanitized).
 
 
-def to_file_name(name):
+def ensure_list(x):
+    if isinstance(x, list):
+        return x
+    return [x]
+
+
+def to_file_name(name: str) -> str:
     """Replaces characters in ``name`` that shouldn't be used for file or
     dir names.
 
@@ -37,39 +47,172 @@ def to_file_name(name):
 
 
 parsed = anymarkup.parse_file('../examples/rgsummary1.xml')['ResourceSummary']
-#anymarkup.serialize_file({"ResourceSummary": parsed}, "rgroundtrip.xml")
-#anymarkup.serialize_file({"ResourceSummary": parsed}, "rgfull.yml")
 
 topology = {}
 
-for rg in parsed['ResourceGroup']:
-    sanfacility = to_file_name(rg['Facility']['Name'])
+
+def simplify_attr_list(data: Union[Dict, List], namekey: str) -> Dict:
+    """
+    Simplify
+        [{namekey: "name1", "attr1": "val1", ...},
+         {namekey: "name2", "attr1": "val1", ...}]}
+    or, if there's only one,
+        {namekey: "name1", "attr1": "val1", ...}
+    to
+      {"name1": {"attr1": "val1", ...},
+       "name2": {"attr1": "val1", ...}}
+    """
+    new_data = {}
+    for d in ensure_list(data):
+        new_d = dict(d)
+        name = new_d[namekey]
+        del new_d[namekey]
+        new_data[name] = new_d
+    return new_data
+
+
+def simplify_services(services):
+    """
+    Simplify Services attribute
+    """
+    new_services = simplify_attr_list(services["Service"], "Name")
+    for service in new_services.values():
+        if service["Details"]:
+            service["Details"] = dict(service["Details"])  # OrderedDict -> dict
+    return new_services
+
+
+def simplify_voownership(voownership):
+    """
+    Simplify VOOwnership attribute
+    """
+    if not isinstance(voownership, dict):
+        return voownership
+    voownership = dict(voownership)  # copy
+    del voownership["ChartURL"]  # can be derived from the other attributes
+    new_voownership = simplify_attr_list(voownership["Ownership"], "VO")
+    for vo in new_voownership:
+        new_voownership[vo] = int(new_voownership[vo]["Percent"])
+    return new_voownership
+
+
+def simplify_contactlists(contactlists):
+    """Simplify ContactLists attribute
+
+    Turn e.g.
+    {"ContactList":
+        [{"ContactType": "Administrative Contact",
+            {"Contacts":
+                {"Contact":
+                    [{"Name": "Andrew Malone Melo", "ContactRank": "Primary"},
+                     {"Name": "Paul Sheldon", "ContactRank": "Secondary"}]
+                }
+            }
+         }]
+    }
+
+    into
+
+    {"Administrative Contact":
+        {"Primary": "Andrew Malone Melo",
+         "Secondary": "Paul Sheldon"}
+    }
+    """
+    if not isinstance(contactlists, dict):
+        return contactlists
+    contactlists_simple = simplify_attr_list(contactlists["ContactList"], "ContactType")
+    new_contactlists = {}
+    for contact_type, contact_data in contactlists_simple.items():
+        contacts = simplify_attr_list(contact_data["Contacts"]["Contact"], "ContactRank")
+        new_contacts = {}
+        for contact_rank in contacts:
+            if contact_rank in new_contacts and contacts[contact_rank]["Name"] != new_contacts[contact_rank]:
+                # Multiple people with the same rank -- hope this never happens.
+                # Duplicates are fine though -- we collapse them into one.
+                raise RuntimeError("dammit %s" % contacts[contact_rank]["Name"])
+            new_contacts[contact_rank] = contacts[contact_rank]["Name"]
+        new_contactlists[contact_type] = new_contacts
+    return new_contactlists
+
+def simplify_resource(res: Dict) -> Dict:
+    res = dict(res)
+
+    res["Services"] = simplify_services(res["Services"])
+    res["VOOwnership"] = simplify_voownership(res["VOOwnership"])
+    if isinstance(res["WLCGInformation"], OrderedDict):
+        res["WLCGInformation"] = dict(res["WLCGInformation"])
+    if isinstance(res["FQDNAliases"], dict):
+        aliases = []
+        for a in ensure_list(res["FQDNAliases"]["FQDNAlias"]):
+            aliases.append(a)
+        res["FQDNAliases"] = aliases
+    res["ContactLists"] = simplify_contactlists(res["ContactLists"])
+
+    return res
+
+
+def simplify_resourcegroup(rg: Dict) -> Dict:
+    """Simplify the data structure in the ResourceGroup.  Returns the simplified ResourceGroup."""
+    rg = dict(rg)
+
+    # {"SupportCenter": {"ID": XXX, "Name": YYY}} -> {"SupportCenterID": XXX, "SupportCenterName": YYY}
+    rg["SupportCenterID"] = rg["SupportCenter"]["ID"]
+    rg["SupportCenterName"] = rg["SupportCenter"]["Name"]
+    del rg["SupportCenter"]
+
+    rg["Resources"] = simplify_attr_list(rg["Resources"]["Resource"], "Name")
+    for key, val in rg["Resources"].items():
+        rg["Resources"][key] = simplify_resource(val)
+
+    return rg
+
+for rg in ensure_list(parsed["ResourceGroup"]):
+    sanfacility = to_file_name(rg["Facility"]["Name"])
     if sanfacility not in topology:
-        topology[sanfacility] = {}
-    sansite = to_file_name(rg['Site']['Name'])
+        topology[sanfacility] = {"ID": rg["Facility"]["ID"]}
+    sansite = to_file_name(rg["Site"]["Name"])
     if sansite not in topology[sanfacility]:
-        topology[sanfacility][sansite] = {}
-    sanrg = to_file_name(rg['GroupName']) + ".yml"
-    topology[sanfacility][sansite][sanrg] = rg
+        topology[sanfacility][sansite] = {"ID": rg["Site"]["ID"]}
+    sanrg = to_file_name(rg["GroupName"]) + ".yml"
+
+    rg_copy = dict(rg)
+    # Get rid of these fields; we're already putting them in the file/dir names.
+    del rg_copy["Facility"]
+    del rg_copy["Site"]
+    del rg_copy["GroupName"]
+
+    try:
+        topology[sanfacility][sansite][sanrg] = simplify_resourcegroup(rg_copy)
+    except Exception:
+        print("*** We were parsing %s/%s/%s" % (sanfacility, sansite, sanrg), file=sys.stderr)
+        pprint.pprint(rg_copy, stream=sys.stderr)
+        print("\n\n", file=sys.stderr)
+        raise
+
 
 topdir = "/tmp/topology"
 if os.path.isdir(topdir):
     shutil.rmtree(topdir)
 os.makedirs(topdir)
-for fkey, fval in topology.items():
-    facilitydir = os.path.join(topdir, fkey)
-    for skey, sval in fval.items():
-        sitedir = os.path.join(facilitydir, skey)
-        os.makedirs(sitedir)
-        for rgkey, rgval in sval.items():
-            anymarkup.serialize_file(rgval, os.path.join(sitedir, rgkey))
+
+for facility_name, facility_data in topology.items():
+    facility_dir = os.path.join(topdir, facility_name)
+    os.makedirs(facility_dir)
+
+    anymarkup.serialize_file({"ID": facility_data["ID"]},
+                             os.path.join(facility_dir, "FACILITY.yml"))
+
+    for site_name, site_data in facility_data.items():
+        if site_name == "ID": continue
+
+        site_dir = os.path.join(facility_dir, site_name)
+        os.makedirs(site_dir)
+
+        anymarkup.serialize_file({"ID": site_data["ID"]}, os.path.join(site_dir, "SITE.yml"))
+
+        for rg_name, rg_data in site_data.items():
+            if rg_name == "ID": continue
+
+            anymarkup.serialize_file(rg_data, os.path.join(site_dir, rg_name))
+
 print("take a look in", topdir)
-# for project in parsed['Projects']['Project']:
-#     print("Would Create file: %s.yaml" % (project['Name']))
-#     serialized = anymarkup.serialize(project, "yaml")
-#     serialized = serialized.replace("!!omap", "").strip()
-#     with open("projects/{0}.yaml".format(project['Name']), 'w') as f:
-#         f.write(serialized)
-#     #print(project)
-
-
--- converters/resourcegroup_yaml_to_xml.py
+++ converters/resourcegroup_yaml_to_xml.py
@@ -1,58 +1,217 @@
+import urllib.parse
+
 import anymarkup
 import os
-from typing import List, Dict
+import pprint
+import sys
+from pathlib import Path
+from typing import Dict, List, Union, Iterable
 
 topdir = "/tmp/topology"
-SCHEMA_LOCATION = b"https://my.opensciencegrid.org/schema/rgsummary.xsd"
+SCHEMA_LOCATION = "https://my.opensciencegrid.org/schema/rgsummary.xsd"
+
+
+class Topology(object):
+    def __init__(self):
+        self.data = {}
+
+    def add_rg(self, facility, site, rg, rgdata):
+        if facility not in self.data:
+            self.data[facility] = {}
+        if site not in self.data[facility]:
+            self.data[facility][site] = {}
+        if rg not in self.data[facility][site]:
+            self.data[facility][site][rg] = rgdata
+
+    def add_facility(self, name, id):
+        if name not in self.data:
+            self.data[name] = {}
+        self.data[name]["ID"] = id
+
+    def add_site(self, facility, name, id):
+        if facility not in self.data:
+            self.data[facility] = {}
+        if name not in self.data[facility]:
+            self.data[facility][name] = {}
+        self.data[facility][name]["ID"] = id
+
+    def pprint(self):
+        for f in self.data:
+            print("[%s %s]" % (f, self.data[f]["ID"]), end=" ")
+            for s in self.data[f]:
+                if s == "ID": continue
+                print("[%s %s]" % (s, self.data[f][s]["ID"]), end=" ")
+                for r in self.data[f][s]:
+                    if r == "ID": continue
+                    print("[%s]" % r)
+                    pprint.pprint(self.data[f][s][r])
+                    print("")
+
+    def serialize(self) -> bytes:
+        rgs = []
+        for fval in self.data.values():
+            for s, sval in fval.items():
+                if s == "ID": continue
+                for r, rval in sval.items():
+                    if r == "ID": continue
+                    rgs.append(rval)
+
+        tree = {"ResourceSummary":
+                    {"@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+                     "@xsi:schemaLocation": SCHEMA_LOCATION,
+                     "ResourceGroup": rgs}}
+        return anymarkup.serialize(tree, "xml")
+
+def singleton_list_to_value(a_list):
+    if len(a_list) == 1:
+        return a_list[0]
+    return a_list
+
+
+def expand_attr_list_single(data: Dict, namekey:str, valuekey: str) -> Union[Dict, List]:
+    """
+    Expand
+        {"name1": "val1",
+         "name2": "val2"}
+    to
+        [{namekey: "name1", valuekey: "val1"},
+         {namekey: "name2", valuekey: "val2"}]
+    or, if there's only one,
+        {namekey: "name1", valuekey: "val1"}
+    """
+    newdata = []
+    for name, value in data.items():
+        newdata.append({namekey: name, valuekey: value})
+    return singleton_list_to_value(newdata)
+
+
+def expand_attr_list(data: Dict, namekey: str) -> Union[Dict, List]:
+    """
+    Expand
+        {"name1": {"attr1": "val1", ...},
+         "name2": {"attr1": "val1", ...}}
+    to
+        [{namekey: "name1", "attr1": "val1", ...},
+         {namekey: "name2", "attr1": "val1", ...}]}
+    or, if there's only one,
+        {namekey: "name1", "attr1": "val1", ...}
+    """
+    newdata = []
+    for name, value in data.items():
+        value = dict(value)
+        value[namekey] = name
+        newdata.append(value)
+    return singleton_list_to_value(newdata)
+
+
+def expand_services(services: Dict) -> Dict:
+    return {"Service": expand_attr_list(services, "Name")}
+
+
+def get_charturl(ownership: Iterable) -> str:
+    chd = ""
+    chl = ""
+
+    for name, percent in ownership:
+        chd += "%s," % percent
+        if name == "(Other)":
+            name = "Other"
+        chl += "%s(%s%%)|" % (percent, name)
+    chd = chd.rstrip(",")
+    chl = chl.rstrip("|")
+
+    query = urllib.parse.urlencode({
+        "chco": "00cc00",
+        "cht": "p3",
+        "chd": "t:" + chd,
+        "chs": "280x65",
+        "chl": chl
+    })
+    return "http://chart.apis.google.com/chart?%s" % query
+
+
+def expand_voownership(voownership: Dict) -> Dict:
+    return {
+        "Ownership": expand_attr_list_single(voownership, "VO", "Percent"),
+        "ChartURL": get_charturl(voownership.items())
+    }
+
+
+def expand_contactlists(contactlists: Dict) -> Dict:
+    new_contactlists = []
+    for contact_type, contact_data in contactlists.items():
+        contact_data = expand_attr_list_single(contact_data, "ContactRank", "Name")
+        new_contactlists.append({"ContactType": contact_type, "Contacts": {"Contact": contact_data}})
+    return {"ContactList": singleton_list_to_value(new_contactlists)}
+
+
+def expand_resource(name: str, res: Dict) -> Dict:
+    res = dict(res)
+
+    res["Services"] = expand_services(res["Services"])
+    if res["VOOwnership"] and isinstance(res["VOOwnership"], dict):
+        res["VOOwnership"] = expand_voownership(res["VOOwnership"])
+    else:
+        res["VOOwnership"] = "(Information not available)"
+    if res["FQDNAliases"]:
+        res["FQDNAliases"] = singleton_list_to_value(
+            [{"FQDNAlias": a} for a in res["FQDNAliases"]]
+        )
+    if res["ContactLists"]:
+        res["ContactLists"] = expand_contactlists(res["ContactLists"])
+    res["Name"] = name
+    if len(res["WLCGInformation"]) == 1:
+        res["WLCGInformation"] = "(Information not available)"
+
+    return res
+
+
+def expand_resourcegroup(rg):
+    rg = dict(rg)  # copy
+
+    rg["SupportCenter"] = {"Name": rg["SupportCenterName"], "ID": rg["SupportCenterID"]}
+    del rg["SupportCenterName"], rg["SupportCenterID"]
+
+    new_resources = []
+    for name, res in rg["Resources"].items():
+        try:
+            res = expand_resource(name, res)
+            new_resources.append(res)
+        except Exception:
+            pprint.pprint(res, stream=sys.stderr)
+            raise
+    rg["Resources"] = {"Resource": singleton_list_to_value(new_resources)}
+
+    return rg
+
 
 paths = []
-for root, dirs, files in os.walk(topdir):
-    for f in files:
-        if f.endswith(".yml"):
-            paths.append(os.path.join(root, f))
+topology = Topology()
 
-resourcegroups = []
-# assumption: resourcegroups are unique between sites
-for p in sorted(paths):
-    rg = anymarkup.parse_file(p)
-    resourcegroups.append(rg)
+root = Path(topdir)
+for facility_path in root.glob("*/FACILITY.yml"):
+    name = facility_path.parts[-2]
+    id_ = anymarkup.parse_file(facility_path)["ID"]
+    topology.add_facility(name, id_)
 
-# This sort key _almost_ matches the ordering the original XML file came in
-# but fails with punctuation.
-resourcegroups.sort(key=lambda rg:rg["GroupName"].lower())
+for site_path in root.glob("*/*/SITE.yml"):
+    facility, name = site_path.parts[-3:-1]
+    id_ = anymarkup.parse_file(site_path)["ID"]
+    topology.add_site(facility, name, id_)
 
+for yaml_path in root.glob("*/*/*.yml"):
+    facility, site, name = yaml_path.parts[-3:]
+    if name == "SITE.yml": continue
 
-def get_max_ids(resourcegroups: List[Dict]) -> Dict[int]:
-    """Get the highest IDs for group, facility, site, and resource in all of the ResourceGroup data."""
-    max_ids = dict(group=-1, facility=-1, site=-1, resource=-1)
-    for rg in resourcegroups:
-        group_id = int(rg["GroupID"])
-        facility_id = int(rg["Facility"]["ID"])
-        site_id = int(rg["Site"]["ID"])
-        max_ids["group"] = max(max_ids["group"], group_id)
-        max_ids["facility"] = max(max_ids["facility"], facility_id)
-        max_ids["site"] = max(max_ids["site"], site_id)
+    name = name.replace(".yml", "")
+    rg = anymarkup.parse_file(yaml_path)
 
-        resource = rg["Resources"]["Resource"]
-        if isinstance(resource, dict):
-            # There's only one of these
-            resource_id = int(resource["ID"])
-            max_ids["resource"] = max(max_ids["resource"], resource_id)
-        else:
-            # More than one: this is a list
-            for res in resource:
-                resource_id = int(res["ID"])
-                max_ids["resource"] = max(max_ids["resource"], resource_id)
+    facility_id = topology.data[facility]["ID"]
+    site_id = topology.data[facility][site]["ID"]
+    rg["Facility"] = {"Name": facility, "ID": facility_id}
+    rg["Site"] = {"Name": site, "ID": site_id}
+    rg["GroupName"] = name
 
-    return max_ids
+    topology.add_rg(facility, site, name, expand_resourcegroup(rg))
 
-
-serialized = anymarkup.serialize({"ResourceSummary": {"ResourceGroup": resourcegroups}}, "xml")
-serialized = \
-    serialized.replace(
-        b"<ResourceSummary>",
-        b"<ResourceSummary xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"%s\">"
-        % SCHEMA_LOCATION)
-with open("rgsummary.xml", "wb") as outfile:
-    outfile.write(serialized)
-print(get_max_ids(resourcegroups))
+print(topology.serialize().decode("utf-8", "ignore"))
