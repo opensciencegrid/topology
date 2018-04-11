4a4e7cbd7fad4fae666d0b940f31aeba3a695f1f
Matyas Selmeci <matyas@cs.wisc.edu>
2018-04-11 15:00:26 -0500

    Get max group, facility, site, and resource IDs
    In case we want to add one of these and want to know what IDs to use.

--- converters/resourcegroup_yaml_to_xml.py
+++ converters/resourcegroup_yaml_to_xml.py
@@ -1,5 +1,6 @@
 import anymarkup
 import os
+from typing import List, Dict
 
 topdir = "/tmp/topology"
 SCHEMA_LOCATION = b"https://my.opensciencegrid.org/schema/rgsummary.xsd"
@@ -20,6 +21,32 @@ for p in sorted(paths):
 # but fails with punctuation.
 resourcegroups.sort(key=lambda rg:rg["GroupName"].lower())
 
+
+def get_max_ids(resourcegroups: List[Dict]) -> Dict[int]:
+    """Get the highest IDs for group, facility, site, and resource in all of the ResourceGroup data."""
+    max_ids = dict(group=-1, facility=-1, site=-1, resource=-1)
+    for rg in resourcegroups:
+        group_id = int(rg["GroupID"])
+        facility_id = int(rg["Facility"]["ID"])
+        site_id = int(rg["Site"]["ID"])
+        max_ids["group"] = max(max_ids["group"], group_id)
+        max_ids["facility"] = max(max_ids["facility"], facility_id)
+        max_ids["site"] = max(max_ids["site"], site_id)
+
+        resource = rg["Resources"]["Resource"]
+        if isinstance(resource, dict):
+            # There's only one of these
+            resource_id = int(resource["ID"])
+            max_ids["resource"] = max(max_ids["resource"], resource_id)
+        else:
+            # More than one: this is a list
+            for res in resource:
+                resource_id = int(res["ID"])
+                max_ids["resource"] = max(max_ids["resource"], resource_id)
+
+    return max_ids
+
+
 serialized = anymarkup.serialize({"ResourceSummary": {"ResourceGroup": resourcegroups}}, "xml")
 serialized = \
     serialized.replace(
@@ -28,3 +55,4 @@ serialized = \
         % SCHEMA_LOCATION)
 with open("rgsummary.xml", "wb") as outfile:
     outfile.write(serialized)
+print(get_max_ids(resourcegroups))
