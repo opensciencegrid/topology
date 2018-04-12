413f8c740e8e880cbc9c19e0dbee140b4e7dd5d7
Derek Weitzel <djw8605@gmail.com>
2018-04-12 14:42:12 -0500

    Fixing yaml to xml formatting

--- converters/project_yaml_to_xml.py
+++ converters/project_yaml_to_xml.py
@@ -14,18 +14,12 @@ def get_projects_xml():
 
     for file in os.listdir("projects"):
         project = anymarkup.parse_file("projects/{0}".format(file))
-        # For some reason, it parses the yaml file as a list of dictionaries
-        # Convert to a big dictionary, so it can output proper xml
-        new_project = {}
-        for attr in project:
-            new_project.update(attr)
-        projects.append(new_project)
+        projects.append(project)
 
 
     to_output["Projects"]["Project"] = projects
 
-
-    return anymarkup.serialize(to_output, 'xml')
+    return anymarkup.serialize(to_output, 'xml').decode()
 
 
 if __name__ == "__main__":
