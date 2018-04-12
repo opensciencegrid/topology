f813faebb5ad3397894ecddd4add22beb391d0a7
Derek Weitzel <djw8605@gmail.com>
2018-04-12 13:18:32 -0500

    Adding converter as well

--- converters/project_yaml_to_xml.py
+++ converters/project_yaml_to_xml.py
@@ -1,23 +1,36 @@
-import anymarkup
 import os
 
-to_output = {"Projects": { "Project": [] } }
-projects = []
-
-for file in os.listdir("projects"):
-    project = anymarkup.parse_file("projects/{0}".format(file))
-    # For some reason, it parses the yaml file as a list of dictionaries
-    # Convert to a big dictionary, so it can output proper xml
-    new_project = {}
-    for attr in project:
-        new_project.update(attr)
-    projects.append(new_project)
-    
-
-to_output["Projects"]["Project"] = projects
-
-#print projects
-anymarkup.serialize_file(to_output, 'new_projects.xml')
+import anymarkup
 
 
+
+def get_projects_xml():
+    """
+    Returns the serailized xml (as a string)
+    """
+
+    to_output = {"Projects":{"Project": []}}
+    projects = []
+
+    for file in os.listdir("projects"):
+        project = anymarkup.parse_file("projects/{0}".format(file))
+        # For some reason, it parses the yaml file as a list of dictionaries
+        # Convert to a big dictionary, so it can output proper xml
+        new_project = {}
+        for attr in project:
+            new_project.update(attr)
+        projects.append(new_project)
+
+
+    to_output["Projects"]["Project"] = projects
+
+
+    return anymarkup.serialize(to_output, 'xml')
+
+
+if __name__ == "__main__":
+    # We are running as the main script
+
+    with open('new_projects.xml', 'w') as xml_file:
+        xml_file.write(get_projects_xml())
 
