6b408b7b302f74820f93cdd68bd31935036bd95e
Matyas Selmeci <matyas@cs.wisc.edu>
2018-04-30 17:13:49 -0500

    Simplify contacts in virtual organizations

--- converters/vo_xml_to_yaml.py
+++ converters/vo_xml_to_yaml.py
@@ -1,5 +1,8 @@
+import pprint
 import xmltodict
 import yaml
+
+from convertlib import is_null, simplify_attr_list, ensure_list
 
 with open('vos.xml', 'r') as vo_xml_file:
     # Use dict_constructore = dict so we don't get ordered dicts, we don't really care about ordering
@@ -16,10 +19,54 @@ def str_presenter(dumper, data):
 yaml.add_representer(str, str_presenter)
 
 
+def simplify_contacttypes(contacttypes):
+    """Simplify ContactTypes attribute
+
+    Turn e.g.
+    {"ContactTypes":
+        {"ContactType":
+            [{"Contacts":
+                {"Contact": [{"Name": "Steve Timm"},
+                             {"Name": "Joe Boyd"}]},
+              "Type": "Miscellaneous Contact"}
+            ]
+        }
+    }
+
+    into
+
+    {"Contacts":
+        {"Miscellanous Contact":
+            [ "Steve Timm", "Joe Boyd" ]
+        }
+    }
+    """
+    if is_null(contacttypes):
+        return None
+    contacttypes_simple = simplify_attr_list(contacttypes["ContactType"], "Type")
+    new_contacts = {}
+
+    for contact_type, contact_data in contacttypes_simple.items():
+        contacts_list = []
+        for contact in ensure_list(contact_data["Contacts"]["Contact"]):
+            if contact["Name"] not in contacts_list:
+                contacts_list.append(contact["Name"])
+        new_contacts[contact_type] = contacts_list
+
+    return new_contacts
+
+
 for vo in parsed['VOSummary']['VO']:
-    print("Would Create file: {0}.yaml".format(vo['Name']))
+    newvo = vo
+    if "ContactTypes" in newvo:
+        newvo["Contacts"] = simplify_contacttypes(newvo["ContactTypes"])
+        del newvo["ContactTypes"]
+
+    # pprint.pprint(newvo)
+
+    print("Would Create file: {0}.yaml".format(newvo['Name']))
     serialized = yaml.safe_dump(vo, encoding='utf-8', default_flow_style=False)
     print(serialized.decode())
-    with open("virtual-organizations/{0}.yaml".format(vo['Name']), 'w') as f:
+    with open("virtual-organizations/{0}.yaml".format(newvo['Name']), 'w') as f:
         f.write(serialized.decode())
 
