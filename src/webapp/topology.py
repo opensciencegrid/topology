0f872b775467332f532b0d5b2fcdd50da836cd0c
Carl Edquist <edquist@cs.wisc.edu>
2018-07-18 10:57:47 -0500

    Revert "Fix sign error for past days downtime filter"
    This reverts commit 7b8a934abcf0c0ca2444749e4d1b91ee68303720.
    
    Because Time Goes Forward.

--- src/webapp/topology.py
+++ src/webapp/topology.py
@@ -303,10 +303,6 @@ class Downtime(object):
 
     @property
     def end_age(self) -> timedelta:
-        """Returns a timedelta instance for the end time of a downtime relative
-        to the current time. Past end times yield a negative timedelta and
-        future end times yield a positive timedelta.
-        """
         current_time = datetime.now(timezone.utc)
         return self.end_time - current_time
 
@@ -324,9 +320,7 @@ class Downtime(object):
             return
         # unlike the other filters, if past_days is not specified, _no_ past downtime is shown
         if filters.past_days >= 0:
-            # Filter out all downtimes that are older than 'past_days' so we swap its sign to account for end_age being
-            # negative for past end times. Always show downtimes with end_ages in the future.
-            if self.end_age.total_seconds() // 86400 < -filters.past_days:
+            if self.end_age.total_seconds() // 86400 < filters.past_days:
                 return
 
         return self._expand_downtime(filters.service_id)
