9a27e7ea76bb415f32bb68897d733ab8891be4d3
Matyas Selmeci <matyas@cs.wisc.edu>
2018-08-14 10:13:38 -0500

    Add common.gen_id()

--- src/webapp/common.py
+++ src/webapp/common.py
@@ -6,7 +6,7 @@ import re
 import shlex
 import subprocess
 import sys
-from typing import Dict, List, Union
+from typing import Dict, List, Union, AnyStr
 
 import xmltodict
 
@@ -193,3 +193,8 @@ def git_clone_or_pull(repo, dir, branch, ssh_key=None) -> bool:
         ok = run_git_cmd(["clone", repo, dir], ssh_key=ssh_key)
         ok = ok and run_git_cmd(["checkout", branch], dir=dir)
     return ok
+
+
+def gen_id(instr: AnyStr, mod=2**64, hashfn=hashlib.md5) -> int:
+    instr_b = instr if isinstance(instr, bytes) else instr.encode("utf-8", "surrogateescape")
+    return int(hashfn(instr_b).hexdigest(), 16) % mod
