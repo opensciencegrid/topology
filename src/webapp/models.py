f004ca2988303e6d8c00d686cb1b1ed3627654f6
Matyas Selmeci <matyas@cs.wisc.edu>
2018-08-17 19:32:34 -0500

    Put yaml generation into models.py

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -1,11 +1,15 @@
+import datetime
 import logging
 import os
 import time
-from typing import Dict, Set
+from typing import Dict, Set, List
+
+import yaml
 
 from webapp import common, contacts_reader, project_reader, rg_reader, vo_reader
+from webapp.common import gen_id
 from webapp.contacts_reader import ContactsData
-from webapp.topology import Topology
+from webapp.topology import Topology, Downtime
 from webapp.vos_data import VOsData
 
 
@@ -135,3 +139,29 @@ class GlobalData:
                 self.projects.try_again()
 
         return self.projects.data
+
+
+def get_downtime_yaml(start_datetime: datetime.datetime,
+                      end_datetime: datetime.datetime,
+                      created_datetime: datetime.datetime,
+                      description: str,
+                      severity: str,
+                      class_: str,
+                      resource_name: str,
+                      services: List[str]) -> str:
+    start_time_str = Downtime.fmttime_preferred(start_datetime)
+    end_time_str = Downtime.fmttime_preferred(end_datetime)
+    created_time_str = Downtime.fmttime_preferred(created_datetime)
+    dtid = gen_id(f"{created_time_str}{resource_name}", digits=11)
+
+    return yaml.dump([{
+        "ID": dtid,
+        "Description": description,
+        "Class": class_,
+        "Severity": severity,
+        "StartTime": start_time_str,
+        "EndTime": end_time_str,
+        "CreatedTime": created_time_str,
+        "ResourceName": resource_name,
+        "Services": services,
+    }], default_flow_style=False)
