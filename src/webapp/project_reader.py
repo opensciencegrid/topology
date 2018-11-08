d4a7af33ffdec6961e9e51b7a27704f9ae4bd41f
Matyas Selmeci <matyas@cs.wisc.edu>
2018-11-07 20:41:40 -0600

    generate IDs if missing for projects

--- src/webapp/project_reader.py
+++ src/webapp/project_reader.py
@@ -11,7 +11,7 @@ import yaml
 if __name__ == "__main__" and __package__ is None:
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from webapp.common import load_yaml_file, to_xml
+from webapp.common import load_yaml_file, to_xml, gen_id
 
 
 log = logging.getLogger(__name__)
@@ -36,6 +36,10 @@ def get_projects(indir="../projects", strict=False):
                 log.error("skipping (non-strict mode)")
                 continue
         project.update(data)
+        if not project["Name"]:
+            project["Name"] = os.path.basename(file)[:-5]
+        if not project["ID"]:
+            project["ID"] = gen_id(project["Name"], digits=9, minimum=1000)
         projects.append(project)
 
     to_output["Projects"]["Project"] = projects
