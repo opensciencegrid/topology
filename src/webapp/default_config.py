f1afa695361fcb1fb60cb1a2f8d5bdb3130163b0
Carl Edquist <edquist@cs.wisc.edu>
2018-12-20 14:04:49 -0600

    update default path for WEBHOOK_DATA_DIR (SOFTWARE-3325)
    in particular,
    
    - it is distinct from TOPOLOGY_DATA_DIR
    - it lives under a different parent dir than TOPOLOGY_DATA_DIR,
      which is owned by a separate user (topomerge)
    - it is a bare repo (thus the .git extension)

--- src/webapp/default_config.py
+++ src/webapp/default_config.py
@@ -5,7 +5,7 @@ TOPOLOGY_DATA_REPO = "https://github.com/opensciencegrid/topology"
 TOPOLOGY_DATA_BRANCH = "master"
 TOPOLOGY_CACHE_LIFETIME = 60 * 15
 
-WEBHOOK_DATA_DIR = "/tmp/topology/topology"
+WEBHOOK_DATA_DIR = "/tmp/topology-webhook/topology.git"
 WEBHOOK_DATA_REPO = "https://github.com/opensciencegrid/topology"
 
 CONTACT_DATA_DIR = "/tmp/topology/contact"
