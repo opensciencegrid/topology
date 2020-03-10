66c6c8c5fd99643cda49cbab65f449fb4a464b15
Carl Edquist <edquist@cs.wisc.edu>
2020-03-10 15:34:41 -0500

    fix docstring for get_vo_oasis_managers

--- src/webapp/oasis_managers.py
+++ src/webapp/oasis_managers.py
@@ -44,7 +44,7 @@ def get_contact_cilogon_id_map(global_data):
 
 
 def get_vo_oasis_managers(global_data, vo):
-    """return OASIS Managers dict for given vo, if any, else an empty dict"""
+    """return OASIS Managers list for given vo, if any, else an empty list"""
     vos_data = global_data.get_vos_data()
     return safe_dict_get(vos_data, vo, "OASIS", "Managers", default=[])
 
