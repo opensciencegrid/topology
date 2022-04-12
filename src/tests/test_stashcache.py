8a4360854de7b3ac7f92150a7001897c49429379
Cannon Lock <clock@wisc.edu>
2022-04-12 15:18:38 -0500

    Remove LIGO DN when no Allowed ( SOFTWARE-5029 )
    Checks for 'ANY' or 'LIGO' adding the ligo dn hashes. Additionally implements tests to confirm these cases and the alternative.

---
+++ src/tests/test_stashcache.py
@@ -0,0 +1,46 @@
+import flask
+import pytest
+from pytest_mock import MockerFixture
+
+# Rewrites the path so the app can be imported like it normally is
+import os
+import sys
+topdir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(topdir)
+
+from app import app, global_data
+import stashcache
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+class TestStachcache:
+
+    def test_allowedVO_includes_ANY_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
+        spy = mocker.spy(stashcache, "_generate_ligo_dns")
+
+        stashcache.generate_cache_authfile(global_data, "sc-cache.chtc.wisc.edu")
+
+        assert spy.call_count == 1
+
+    def test_allowedVO_includes_LIGO_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
+        spy = mocker.spy(stashcache, "_generate_ligo_dns")
+
+        stashcache.generate_cache_authfile(global_data, "stashcache.gravity.cf.ac.uk")
+
+        assert spy.call_count == 1
+
+    def test_allowedVO_excludes_LIGO_and_ANY_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
+        spy = mocker.spy(stashcache, "_generate_ligo_dns")
+
+        stashcache.generate_cache_authfile(global_data, "rds-cache.sdsc.edu")
+
+        assert spy.call_count == 0
+
+
+
+if __name__ == '__main__':
+    pytest.main()
