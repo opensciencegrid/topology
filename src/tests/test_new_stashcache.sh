e37fe68cf1fca5ff6a0fd97e20fa483f2fbc601f
Matyas Selmeci <matyas@cs.wisc.edu>
2022-06-09 18:18:33 -0500

    Test cases for the new stashcache schema

---
+++ src/tests/test_new_stashcache.sh
@@ -0,0 +1,65 @@
+#!/bin/zsh
+
+test_topology=http://localhost:5000
+prod_topology=https://topology.opensciencegrid.org
+
+cache_args=(
+    ""
+    "?fqdn=sc-cache.chtc.wisc.edu"
+    "?fqdn=stash-cache.osg.chtc.io"
+    "?fqdn=dummy.example.net"
+    "?fqdn=osg.kans.nrp.internet2.edu"
+)
+
+cache_endpoints=(
+    "cache/Authfile"
+    "cache/Authfile-public"
+    "cache/scitokens.conf"
+)
+
+origin_args=(
+    ""
+    "?fqdn=sc-origin.chtc.wisc.edu"
+    "?fqdn=sc-origin2000.chtc.wisc.edu"
+    "?fqdn=origin.ligo.caltech.edu"
+    "?fqdn=hcc-stashcache-origin.unl.edu"
+)
+
+origin_endpoints=(
+    "origin/Authfile"
+    "origin/Authfile-public"
+    "origin/scitokens.conf"
+)
+
+oldfile=/tmp/stashcachetest-prod.txt
+newfile=/tmp/stashcachetest-test.txt
+truncate -s0 $oldfile
+truncate -s0 $newfile
+for endpoint in "${cache_endpoints[@]}"
+do
+    for arg in "${cache_args[@]}"
+    do
+        url=$endpoint$arg
+        printf "%s\n\n\n" "$url" >> $oldfile
+        printf "%s\n\n\n" "$url" >> $newfile
+        curl -L "$prod_topology/$url" >> $oldfile
+        curl -L "$test_topology/$url" >> $newfile
+    done
+done
+
+printf "\n\n\n\n" >> $oldfile
+printf "\n\n\n\n" >> $newfile
+
+for endpoint in "${origin_endpoints[@]}"
+do
+    for arg in "${origin_args[@]}"
+    do
+        url=$endpoint$arg
+        printf "%s\n\n\n" "$url" >> $oldfile
+        printf "%s\n\n\n" "$url" >> $newfile
+        curl -L "$prod_topology/$url" >> $oldfile
+        curl -L "$test_topology/$url" >> $newfile
+    done
+done
+
+diff -u $oldfile $newfile
