9c35ba8d19478e8a0dc0d3122b24fcdee9a6f324
Matyas Selmeci <matyas@cs.wisc.edu>
2022-06-08 23:39:41 -0500

    Use the new functions in test_stashcache.py

--- src/tests/test_stashcache.py
+++ src/tests/test_stashcache.py
@@ -22,26 +22,26 @@ class TestStashcache:
     def test_allowedVO_includes_ANY_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
         spy = mocker.spy(stashcache, "_generate_ligo_dns")
 
-        stashcache.generate_cache_authfile(global_data, "sc-cache.chtc.wisc.edu")
+        stashcache.generate_cache_authfile2("sc-cache.chtc.wisc.edu", global_data)
 
         assert spy.call_count == 1
 
     def test_allowedVO_includes_LIGO_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
         spy = mocker.spy(stashcache, "_generate_ligo_dns")
 
-        stashcache.generate_cache_authfile(global_data, "stashcache.gravity.cf.ac.uk")
+        stashcache.generate_cache_authfile2("stashcache.gravity.cf.ac.uk", global_data)
 
         assert spy.call_count == 1
 
     def test_allowedVO_excludes_LIGO_and_ANY_for_ligo_inclusion(self, client: flask.Flask, mocker: MockerFixture):
         spy = mocker.spy(stashcache, "_generate_ligo_dns")
 
-        stashcache.generate_cache_authfile(global_data, "rds-cache.sdsc.edu")
+        stashcache.generate_cache_authfile2("rds-cache.sdsc.edu", global_data)
 
         assert spy.call_count == 0
 
     def test_None_fdqn_isnt_error(self, client: flask.Flask):
-        stashcache.generate_cache_authfile(global_data, None)
+        stashcache.generate_cache_authfile2(None, global_data)
 
 
 if __name__ == '__main__':
