805775ffc9f0085a91953cc3ce520e4e0c5a7c71
Matyas Selmeci <matyas@cs.wisc.edu>
2022-06-30 10:13:01 -0500

    Add the `/stashcache/namespaces.json` endpoint to make testing easier

--- src/app.py
+++ src/app.py
@@ -375,6 +375,7 @@ def scitokens():
 
 
 @app.route("/stashcache/namespaces")
+@app.route("/stashcache/namespaces.json")  # for testing; remove before merging
 def stashcache_namespaces_json():
     if not stashcache:
         return Response("Can't get scitokens config: stashcache module unavailable", status=503)
