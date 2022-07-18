8e4cabf06dc3a4141a8781d37304f086ea85f69c
Carl Edquist <edquist@cs.wisc.edu>
2022-07-18 14:42:33 -0500

    pull in safe_dict_get from webapp/common.py (SOFTWARE-4167)
    I started getting uncomfortable reaching way into a dict path for
    objects, and carefully checking was getting annoying

--- src/tests/verify_resources.py
+++ src/tests/verify_resources.py
@@ -28,6 +28,21 @@ class autodict(collections.defaultdict):
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__, dict.__repr__(self))
 
+
+# from webapp/common.py
+def safe_dict_get(item, *keys, default=None):
+    """ traverse dict hierarchy without producing KeyErrors:
+        safe_dict_get(item, key1, key2, ..., default=default)
+        -> item[key1][key2][...] if defined and not None, else default
+    """
+    for key in keys:
+        if isinstance(item, dict):
+            item = item.get(key)
+        else:
+            return default
+    return default if item is None else item
+
+
 def rgfilter(fn):
     return not (fn.endswith("_downtime.yaml") or fn.endswith("/SITE.yaml"))
 
