696c95562148920d9465d0c36ca1f478297adec5
Matyas Selmeci <matyas@cs.wisc.edu>
2022-07-21 18:44:16 -0500

    Add a config file for running the webapp in a GHA

---
+++ src/config-ci.py
@@ -0,0 +1,29 @@
+# config file for CI testing development
+
+
+import os
+
+# The parent directory of this file, i.e. the current checkout:
+TOPOLOGY_DATA_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# If you use this checkout for TOPOLOGY_DATA_DIR, be sure to set NO_GIT=True,
+# otherwise your changes will get blown away!
+NO_GIT = True
+
+
+# If you have access to contact DB, point this to a checkout of it.
+CONTACT_DATA_DIR = "/tmp/contact"
+
+TOPOLOGY_CACHE_LIFETIME = 999
+CONTACT_CACHE_LIFETIME = 999
+
+INSTANCE_NAME = "CI testing"
+
+# Set this to False to skip querying LIGO's LDAP servers when generating
+# StashCache information.
+STASHCACHE_LEGACY_AUTH = False
+
+import logging
+LOGLEVEL = logging.DEBUG  # flask's default is WARNING
+
+# Be authorized.  Does nothing if you don't have any contacts data.
+AUTH = True
