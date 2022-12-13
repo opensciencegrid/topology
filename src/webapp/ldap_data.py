443a30fc54ec3a4eac5a743fa0f91ce745a310a1
Carl Edquist <edquist@cs.wisc.edu>
2022-12-13 17:12:30 -0600

    retrieve first github username - ldap returns a list (SOFTWARE-5313)

--- src/webapp/ldap_data.py
+++ src/webapp/ldap_data.py
@@ -75,7 +75,7 @@ def cilogon_id_map_to_yaml_data(m):
 
             github = entry['data'].get('voPersonExternalID')
             if github:
-                data[id_]['GitHub'] = github
+                data[id_]['GitHub'] = github[0]
 
     return data
 
