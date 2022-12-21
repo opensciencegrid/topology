1f337fbf59adf661dc4b91d3aca34d1eec574d14
Brian Lin <brianhlin@gmail.com>
2022-12-21 17:40:36 -0600

    Rename class to avoid future typos

--- src/topology_utils.py
+++ src/topology_utils.py
@@ -25,7 +25,7 @@ class InvalidPathError(AuthError):
 class IncorrectPasswordError(AuthError):
     pass
 
-class OSGPoolManager(urllib3.PoolManager):
+class TopologyPoolManager(urllib3.PoolManager):
 
     def __init__(self):
         self.session = False
