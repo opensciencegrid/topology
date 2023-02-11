1608a7801fa5587f1de95996b74170c1a8ecf9b0
Matyas Selmeci <matyas@cs.wisc.edu>
2023-02-10 18:23:21 -0600

    Mock some DNs and hashes
    At first I tried mocking `ldap_data.get_ligo_ldap_dn_list()` but that doesn't work because `global_data.get_ligo_dn_list()` won't even call it without the credentials.

--- src/tests/test_stashcache.py
+++ src/tests/test_stashcache.py
@@ -17,9 +17,23 @@ GRID_MAPPING_REGEX = re.compile(r'^"(/[^"]*CN=[^"]+")\s+([0-9a-f]{8}[.]0)$')
 # ^^ the DN starts with a slash and will at least have a CN in it.
 EMPTY_LINE_REGEX = re.compile(r'^\s*(#|$)')  # Empty or comment-only lines
 I2_TEST_CACHE = "osg-sunnyvale-stashcache.t2.ucsd.edu"
-
-
 # ^^ one of the Internet2 caches; these serve both public and LIGO data
+
+
+# Some DNs I can use for testing and the hashes they map to.
+# All of these were generated with osg-ca-generator on alma8 and printed with
+#   openssl x509 -in /etc/grid-security/hostcert.pem -noout -subject -subject_hash_old -nameopt compat
+# (trailing ".0" added by hand)
+MOCK_DNS_AND_HASHES = {
+    "/DC=org/DC=opensciencegrid/C=US/O=OSG Software/OU=Services/CN=testhost1": "d460d8b7.0",
+    "/DC=org/DC=opensciencegrid/C=US/O=OSG Software/OU=Services/CN=testhost2": "23c91cd1.0",
+    "/DC=org/DC=opensciencegrid/C=US/O=OSG Software/OU=Services/CN=testhost3": "fd626fa1.0",
+    "/DC=org/DC=opensciencegrid/C=US/O=OSG Software/OU=Services/CN=testhost4": "cc84ae86.0",
+    "/DC=org/DC=opensciencegrid/C=US/O=OSG Software/OU=Services/CN=testhost5": "8967f473.0",
+}
+
+MOCK_DN_LIST = list(MOCK_DNS_AND_HASHES.keys())
+
 
 @pytest.fixture
 def client():
@@ -70,7 +84,8 @@ class TestStashcache:
                 assert False, 'Unexpected text "%s".\nFull text:\n%s' % (line, test_origin_text)
         assert num_mappings > 5, "Too few mappings found.\nFull text:\n%s" % test_origin_text
 
-    def test_cache_authfile(self, client: flask.Flask):
+    def test_cache_authfile(self, client: flask.Flask, mocker: MockerFixture):
+        m = mocker.patch.object(global_data, "get_ligo_dn_list", spec={"return_value": MOCK_DN_LIST})
         text = stashcache.generate_cache_authfile(
             global_data,
             I2_TEST_CACHE,
@@ -80,7 +95,7 @@ class TestStashcache:
 
         assert False, f"Cache authfile dump:\n{text}"
 
-    def test_cache_grid_mapfile(self, client: flask.Flask):
+    def test_cache_grid_mapfile(self, client: flask.Flask, mocker: MockerFixture):
         nohost_text = stashcache.generate_cache_grid_mapfile(global_data, "", legacy=False, suppress_errors=False)
         for line in nohost_text.split("\n"):
             if EMPTY_LINE_REGEX.match(line):
@@ -97,6 +112,7 @@ class TestStashcache:
             else:
                 assert False, 'Unexpected text "%s".\nFull text:\n%s' % (line, nohost_text)
 
+        m = mocker.patch.object(global_data, "get_ligo_dn_list", spec={"return_value": MOCK_DN_LIST})
         test_cache_text = stashcache.generate_cache_grid_mapfile(global_data,
                                                                  I2_TEST_CACHE,
                                                                  legacy=True,
