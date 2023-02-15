062318500945b18652d21c34b756cd1ea0b49ab4
Matyas Selmeci <matyas@cs.wisc.edu>
2023-02-15 13:58:17 -0600

    Start adding test for generate_origin_grid_mapfile() and generate_cache_grid_mapfile()

--- src/tests/test_stashcache.py
+++ src/tests/test_stashcache.py
@@ -1,5 +1,6 @@
 import flask
 import pytest
+import re
 from pytest_mock import MockerFixture
 
 # Rewrites the path so the app can be imported like it normally is
@@ -10,6 +11,14 @@ sys.path.append(topdir)
 
 from app import app, global_data
 import stashcache
+
+GRID_MAPPING_REGEX = re.compile(r'^"/[^"]*CN=[^"]+"\s+[0-9a-f]{8}[.]0$')
+# ^^ the DN starts with a slash and will at least have a CN in it.
+EMPTY_LINE_REGEX = re.compile(r'^\s*(#|$)')  # Empty or comment-only lines
+I2_TEST_CACHE = "osg-sunnyvale-stashcache.t2.ucsd.edu"
+
+
+# ^^ one of the Internet2 caches; these serve both public and LIGO data
 
 @pytest.fixture
 def client():
@@ -42,6 +51,42 @@ class TestStashcache:
 
     def test_None_fdqn_isnt_error(self, client: flask.Flask):
         stashcache.generate_cache_authfile(global_data, None)
+
+    def test_origin_grid_mapfile(self, client: flask.Flask):
+        nohost_text = stashcache.generate_origin_grid_mapfile(global_data, "", suppress_errors=False)
+        for line in nohost_text.split("\n"):
+            assert EMPTY_LINE_REGEX.match(line)
+
+        test_origin_text = stashcache.generate_origin_grid_mapfile(global_data, "origin-auth2001.chtc.wisc.edu",
+                                                                   suppress_errors=False)
+        num_mappings = 0
+        for line in test_origin_text.split("\n"):
+            if EMPTY_LINE_REGEX.match(line):
+                continue
+            elif GRID_MAPPING_REGEX.match(line):
+                num_mappings += 1
+            else:
+                assert False, 'Unexpected text "%s".\nFull text:\n%s' % (line, test_origin_text)
+        assert num_mappings > 5, "Too few mappings found.\nFull text:\n%s" % test_origin_text
+
+    def test_cache_grid_mapfile(self, client: flask.Flask):
+        nohost_text = stashcache.generate_cache_grid_mapfile(global_data, "", legacy=False, suppress_errors=False)
+        for line in nohost_text.split("\n"):
+            assert EMPTY_LINE_REGEX.match(line), 'Unexpected text "%s".\nFull text:\n%s' % (line, nohost_text)
+
+        test_cache_text = stashcache.generate_cache_grid_mapfile(global_data,
+                                                                 I2_TEST_CACHE,
+                                                                 legacy=True,
+                                                                 suppress_errors=False)
+        num_mappings = 0
+        for line in test_cache_text.split("\n"):
+            if EMPTY_LINE_REGEX.match(line):
+                continue
+            elif GRID_MAPPING_REGEX.match(line):
+                num_mappings += 1
+            else:
+                assert False, 'Unexpected text "%s".\nFull text:\n%s' % (line, test_cache_text)
+        assert num_mappings > 5, "Too few mappings found.\nFull text:\n%s" % test_cache_text
 
 
 if __name__ == '__main__':
