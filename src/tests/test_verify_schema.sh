44a96535fe47657643de7f55d4156b1a3e7aee43
Matyas Selmeci <matyas@cs.wisc.edu>
2023-06-27 13:25:36 -0500

    Replace bitbucket.org host key
    bitbucket.org changed their RSA SSH host key; here is their ECDSA one instead.

--- src/tests/test_verify_schema.sh
+++ src/tests/test_verify_schema.sh
@@ -27,7 +27,7 @@ if [[ $GH_EVENT == 'push' && \
     touch ~/.ssh/known_hosts
     chmod 0600 ~/.ssh/known_hosts
     cat >> ~/.ssh/known_hosts <<EOF
-bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw==
+bitbucket.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPIQmuzMBuKdWeF4+a2sjSSpBK0iqitSQ+5BM9KhpexuGt20JpTVM7u5BDZngncgrqDMbWdxMWWOGtZ9UgbqgZE=
 EOF
 
     touch contacts
