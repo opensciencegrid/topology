e41fb2f0d64f20648c655616652d612f5928e021
Cannon Lock <clock@wisc.edu>
2025-04-22 13:59:26 -0500

    Fix Topology InstitutionId's and update script to prevent future grievances

--- .github/scripts/check_facility_has_institution_id/main.py
+++ .github/scripts/check_facility_has_institution_id/main.py
@@ -22,6 +22,18 @@ def check_facility_institution_id(yaml_string: str):
     if facility['InstitutionID'] not in topology_institution_ids and facility['InstitutionID'] is not None:
         raise Exception(f"Invalid InstitutionID: {facility['InstitutionID']}")
 
+    # If the institution id is None, then the facility should not have any resources
+    if facility['InstitutionID'] is None:
+
+        # Check that the facility does not have any resources
+        # If files exist that are not 'SITE.yaml', then the facility is assumed to have resources
+        folder = "/".join(yaml_string.name.split('/')[:-1])
+        files_in_folder = glob.glob(f"{folder}/**/*")
+        resource_files_exist = any([f for f in files_in_folder if not f.endswith("SITE.yaml")])
+
+        if resource_files_exist:
+            raise Exception("FACILITY.yaml doesn't have a InstitutionID but has resources")
+
 def provide_human_check_interface(facility_files: list):
 
     facility_institution_mapping = {}
@@ -50,6 +62,15 @@ def main():
                 check_facility_institution_id(f)
             except Exception as e:
                 errors.append((file.split("/")[-2], e))
+
+    # Check that we have the same amount of facility filhttps://osg-htc.org/iid/52f5piuly2gges as topology facilities
+    # This prevents the omission of a facility
+    topo_facility_folders = glob.glob("../../../topology/*/")
+    if len(topo_facility_folders) != len(facility_files):
+        topo_facility_folders = set([x + "/FACILITY.yaml" for x in topo_facility_folders])
+        facility_files = set(facility_files)
+        diff = topo_facility_folders - facility_files
+        errors.append(("FACILITY.yaml", f"Number of FACILITY.yaml files does not match number of topology facilities. Missing files: {diff}"))
 
     # Print the errors and exit if needed
     if errors:
